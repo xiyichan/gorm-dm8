e0471fefd5febdb343c84a4985c06ce37d9e8a13
chenlifeng <chenlifeng0995@wezhuiyi.com>
2021-12-13 17:20:18 +0800

    dm8

--- namer.go
+++ namer.go
@@ -1,8 +1,8 @@
 package gorm_dm8
 
 import (
+	"fmt"
 	"gorm.io/gorm/schema"
-	"log"
 	"strings"
 )
 
@@ -15,25 +15,27 @@ func ConvertNameToFormat(x string) string {
 }
 
 func (n Namer) TableName(table string) (name string) {
-	log.Println(ConvertNameToFormat(n.NamingStrategy.TableName(table)))
+	fmt.Println(ConvertNameToFormat(n.NamingStrategy.TableName(table)))
 	return ConvertNameToFormat(n.NamingStrategy.TableName(table))
 }
 
 func (n Namer) ColumnName(table, column string) (name string) {
-	log.Println(ConvertNameToFormat(n.NamingStrategy.ColumnName(table, column)))
+	fmt.Println(ConvertNameToFormat(n.NamingStrategy.ColumnName(table, column)))
 	return ConvertNameToFormat(n.NamingStrategy.ColumnName(table, column))
 }
 
 func (n Namer) JoinTableName(table string) (name string) {
+	fmt.Println(ConvertNameToFormat(n.NamingStrategy.JoinTableName(table)))
 	return ConvertNameToFormat(n.NamingStrategy.JoinTableName(table))
 }
 
 func (n Namer) RelationshipFKName(relationship schema.Relationship) (name string) {
+	fmt.Println(ConvertNameToFormat(n.NamingStrategy.RelationshipFKName(relationship)))
 	return ConvertNameToFormat(n.NamingStrategy.RelationshipFKName(relationship))
 }
 
 func (n Namer) CheckerName(table, column string) (name string) {
-	log.Println(ConvertNameToFormat(n.NamingStrategy.CheckerName(table, column)))
+	fmt.Println(ConvertNameToFormat(n.NamingStrategy.CheckerName(table, column)))
 	return ConvertNameToFormat(n.NamingStrategy.CheckerName(table, column))
 }
 
